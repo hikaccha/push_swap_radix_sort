399f562141c5375c3286ef89fec744504a8634e9
hikaru <ichi-hikaru@i.softbank.jp>
2025-07-06 23:57:46 +0900

    add push.c

---
+++ push.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ichikawahikaru <ichikawahikaru@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 23:21:05 by ichikawahik       #+#    #+#             */
+/*   Updated: 2025/07/06 23:54:19 by ichikawahik      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void    push(t_stack **stack_from, t_stack **stack_to)
+{
+    t_stack *tmp;
+    
+    if (stack_from == NULL || *stack_from == NULL)
+        return (0);
+    tmp = *stack_from;
+    *stack_from = (*stack_from)->next;
+    if(stack_to == NULL)
+        *stack_to = tmp;
+    else
+    {
+        tmp->next = *stack_to;
+        *stack_to = tmp;
+    }
+}
+
+void    push_a(t_stack **stack_a, t_stack **stack_b)
+{
+    push(stack_b, stack_a);
+    write(1, "pa\n", 3);
+}
+
+void    push_b(t_stack **stack_a, t_stack **stack_b)
+{
+    push(stack_a, stack_b);
+    write(1, "pb\n", 3);
+}
