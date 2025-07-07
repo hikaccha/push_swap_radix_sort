8c6c3d6e98b546be642dc6d868968db890da5969
hikaru <ichi-hikaru@i.softbank.jp>
2025-07-08 01:13:58 +0900

    add free.c file.

---
+++ free.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ichikawahikaru <ichikawahikaru@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 01:07:24 by ichikawahik       #+#    #+#             */
+/*   Updated: 2025/07/08 01:10:53 by ichikawahik      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void    ft_free(char **tmp_array)
+{
+    int i;
+
+    i = 0;
+    while (tmp_array[i])
+    {
+        free(tmp_array[i]);
+        i++;
+    }
+    free(tmp_array);
+}
+
+void    free_stack(t_stack **head)
+{
+    t_stack *tmp;
+    
+    while (*head)
+    {
+        tmp = *head;
+        *head = (*head)->next;
+        free(tmp);
+    }
+}
