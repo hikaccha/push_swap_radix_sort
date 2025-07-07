1641c063019e5bf73a728b9cf75c891e757a32b7
hikaru <ichi-hikaru@i.softbank.jp>
2025-07-07 15:05:16 +0900

    add swap.c file.

---
+++ swap.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ichikawahikaru <ichikawahikaru@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 23:56:15 by ichikawahik       #+#    #+#             */
+/*   Updated: 2025/07/07 15:04:37 by ichikawahik      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void    swap(t_stack **head)
+{
+    t_stack *tmp;
+
+    if (*head == NULL || (*head)->next == NULL)
+        return (0);
+    tmp = (*head)->next;
+    (*head)->next = tmp->next;
+    tmp->next = *head;
+    *head = tmp;
+}
+
+void    sa(t_stack **stack_a)
+{
+    swap(stack_a);
+    write(1, "sa\n", 3);
+}
+
+void    sb(t_stack **stack_b)
+{
+    swap(stack_b);
+    write(1, "sb\n", 3);
+}
+
+void    ss(t_stack **stack_a, t_stack **stack_b)
+{
+    swap(stack_a);
+    swap(stack_b);
+    write(1, "ss\n", 3);
+}
