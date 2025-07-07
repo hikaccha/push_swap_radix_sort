86c226018157aed6956d638cb3f6e087159f9964
hikaru <ichi-hikaru@i.softbank.jp>
2025-07-07 14:50:31 +0900

    add rotate.c file.

---
+++ rotate.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ichikawahikaru <ichikawahikaru@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 23:58:04 by ichikawahik       #+#    #+#             */
+/*   Updated: 2025/07/07 14:48:42 by ichikawahik      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void    rotate(t_stack **head)
+{
+    t_stack *last;
+    
+    if (*head ==NULL || (*head)->next == NULL)
+        return (0);
+    last = *head;
+    while (last->next != NULL)
+        last = last->next;
+    last->next = *head;
+    *head = (*head)->next;
+    last->next->next = NULL;
+}
+
+void    ra(t_stack **stack_a)
+{
+    rotate(stack_a);
+    write(1, "ra\n", 3);
+}
+
+void    rb(t_stack *stack_b)
+{
+    rotate(stack_b);
+    write(1, "rb\n", 3);
+}
+
+void    rr(t_stack **stack_a, t_stack **stack_b)
+{
+    rotate(stack_a);
+    rotate(stack_b);
+    write(1, "rr\n", 3);
+}
