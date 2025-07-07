1046c135f59d9dd26668c0c3c802d6bc5474e195
hikaru <ichi-hikaru@i.softbank.jp>
2025-07-08 00:03:17 +0900

    add sort.c file.

---
+++ sort.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ichikawahikaru <ichikawahikaru@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/07 15:07:46 by ichikawahik       #+#    #+#             */
+/*   Updated: 2025/07/07 19:04:14 by ichikawahik      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void    sort_three(t_stack **head)
+{
+    int biggest;
+    
+    biggest = find_biggest(head);
+    if ((*head)->index == biggest)
+        ra(head);
+    else if ((*head)->next->index == biggest)
+        rra(head);
+    if ((*head)->index > (*head)->next->index)
+        sa(head);
+}
+
+void    sort_five(t_stack **stack_a, t_stack **stack_b)
+{
+    int size;
+
+    size = count_nodes(stack_a);
+    while (size--)
+    {
+        if ((*stack_a)->index == 0 || (*stack_a)->index == 1)
+            pb(stack_a, stack_b);
+        else
+            ra(stack_a);
+    }
+    sort_three(stack_a);
+    pa(stack_a, stack_b);
+    pa(stack_a, stack_b);
+    if ((*stack_a)->index > (*stack_a)->next->index)
+        sa(stack_a);
+}
+
+void    radix_sort(t_stack **stack_a, t_stack **stack_b)
+{
+    int biggest_nbr;
+    int max_bits;
+    int i;
+    int j;
+
+    biggest_nbr = find_biggest(stack_a);
+    max_bits = find_bits(biggest_nbr);
+    i = 0;
+    while (i < max_bits)
+    {
+        j = 0;
+        while (j <= biggest_nbr)
+        {
+            if (((*stack_a)->index >> i) & 1)
+                ra(stack_a);
+            else
+                pb(stack_a, stack_b);
+            j++;
+        }
+        while (*stack_b)
+            pa(stack_a, stack_b);
+        i++;
+    }
+}
+
+void    sort_stack(t_stack **stack_a, t_stack **stack_b)
+{
+    int size;
+
+    size = count_nodes(stack_a);
+    if (!sorted(stack_a) && size <= 3)
+        sort_three(stack_a);
+    else if (!sorted(stack_a) && size <= 5)
+        sort_five(stack_a, stack_b);
+    else if (!sorted(stack_a) && size > 5)
+        radix_sort(stack_a, stack_b);
+    else
+    {
+        free_stack(stack_a);
+        free_stack(stack_b);
+    }
+}
